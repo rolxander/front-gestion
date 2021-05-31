94e589e502ca7e1cc7eeb5917fe52747ddf50199
rolxander <josechoqueserrano@gmail.com>
2021-05-31 06:02:07 -0400

    base64.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import React, { useState } from 'react'
+const axios = require('axios')
+
+function App(){
+  const [data,setData] = useState([])
+  const converterBase64 = ( archivos ) =>{
+    let Files = []
+  //  console.log(archivos)
+    Array.from( archivos ).forEach( archivo =>{
+      let reader = new FileReader();
+      reader.readAsDataURL( archivo );
+      reader.onload = function (){
+        let arrayAuxiliar = []; 
+        var base64 = reader.result;
+        let data_text = base64.split(',');
+        arrayAuxiliar.push(data_text[1])
+        let info = archivo["name"].split(".")
+        arrayAuxiliar.push(info[0])
+        arrayAuxiliar.push(info[1])
+        Files.push(arrayAuxiliar);
+        setData(...data,Files)
+    //    console.log(data)
+      }
+    })
+  }
+  return(<>
+    <div className = "App">
+      <input type="text"/>
+      <input type = "file" multiple onChange ={(e)=>{
+        converterBase64(e.target.files)
+        
+        }}/>
+      <button onClick = {(e) =>{
+      
+        e.preventDefault();
+        console.log(data)
+        let send = false
+        Object.keys(data).map((key)=>{
+          if(data[key][2]  === "pdf" || data[key][2]  === "docx" || data[key][2]  === "txt"  ){
+            send = true
+          } 
+          return  null
+        });
+        if(!send){
+          console.log("no se puede enviar el archivo no pertenece a los formatos aceptados")
+        }else{
+          axios({
+            method:'POST',
+            url:'http://localhost:4000/api',
+            data: data,
+            headers: { 'content-type': 'application/json'}
+
+   })
+   .then((response)=>{
+      console.log(response)
+   }) 
+   .catch((err)=>{
+
+    console.log(err)
+   });
+
+        }
+      }}>submit</button>
     </div>
-  );
+  </>)
 }
-
 export default App;
---
+++ src/components/elements/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+function Elements({path,title}){
+    return (
+    <>
+        <div>
+            <p>ubicacion: {path}</p>
+            <p>tittulo : {title}</p>
+        </div>
+    </>)
+}
+export default Elements
